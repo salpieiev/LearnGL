65b9f76825a83a141cfd1aaf9072d58302252187
Sergey Alpeev <sergey.alpeev@gmail.com>
2013-04-11 17:24:19 +0300

    Added new files

---
+++ LearnGL/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  LearnGL
+//
+//  Created by Sergey Alpeev on 26.12.12.
+//  Copyright (c) 2012 Sergey Alpeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ LearnGL/main.mm
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LearnGL
+//
+//  Created by Sergey Alpeev on 26.12.12.
+//  Copyright (c) 2012 Sergey Alpeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
