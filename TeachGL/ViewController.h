36942313bd9a2f382326515b89043ac12ea8c2a7
Sergey Alpeev <sergey.alpeev@gmail.com>
2013-04-11 17:17:58 +0300

    Revert "Renamed project"
    This reverts commit c3d3654f8c9da07dc2eaf6f5f4ad7181f347b6c1.

---
+++ TeachGL/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  TeachGL
+//
+//  Created by Sergey Alpeev on 26.12.12.
+//  Copyright (c) 2012 Sergey Alpeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+
+
+@interface ViewController : GLKViewController
+
+
+
+@end
